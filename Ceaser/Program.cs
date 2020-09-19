8a01b44b69a6f88769400d61fa5eda35b30e6eec
Асылжан Жансейт <Asilzhan.asa.j@gmail.com>
2020-09-19 19:10:47 +0600

    Initial commit

---
+++ Ceaser/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Security;
+using System.Text;
+
+namespace Ceaser
+{
+    internal class Program
+    {
+        static string alphabet = "аәбвгғдежзийкқлмнңопрстуұүфхрцчшщъыіьэюя,. ";
+
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            ShowInfo();
+            Console.Write(">");
+            string command = Console.ReadLine();
+            while (command != "quit")
+            {
+                Console.WriteLine("Мәтінді енгізіңіз:");
+                string text = Console.ReadLine();
+                Console.WriteLine("Кілтті енгізіңіз");
+                int key = int.Parse(Console.ReadLine());
+                EncryptParameter parameter = (EncryptParameter) int.Parse(command);
+                Console.WriteLine(Encrypt(text, key, parameter));
+                Console.Write(">");
+                command = Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+
+        static string Encrypt(string text, int key, EncryptParameter parameter)
+            {
+                if (parameter == EncryptParameter.Decrypt)
+                    key *= -1;
+                string res = "";
+                for (int i = 0; i < text.Length; i++)
+                {
+                    int pos = alphabet.IndexOf(char.ToLower(text[i]));
+                    pos = (pos + key + alphabet.Length) % alphabet.Length;
+                    res += alphabet[pos];
+                }
+
+                return res;
+            }
+
+            static void ShowInfo()
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(@"[0]-Шифрлау
+[1]-Кері шифрлау
+[quit]-шығу");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        enum EncryptParameter
+        {
+            Encrypt = 0,
+            Decrypt = 1
+        }
+}
